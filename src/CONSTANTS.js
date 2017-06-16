6ce4de9d1d7618db6daddf5ad2ca7757a8c74c8c
Drew Miley <drewmiley92@gmail.com>
2017-06-17 00:43:49 +0100

    Http -> https

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,5 +1,5 @@
 export const API_KEY = 'qUnEyRXt';
-export const BASE_URL = 'http://www.cineworld.com/api/';
+export const BASE_URL = 'https://www.cineworld.com/api/';
 export const FILMS_URL = 'quickbook/films';
 export const PERFORMANCES_URL = 'performances';
 export const CINEMA_ID = 112;
