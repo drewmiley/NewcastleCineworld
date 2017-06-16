b61519b30efad2fa8de840c49395e9db1dbf42c9
Drew Miley <drewmiley92@gmail.com>
2017-06-16 22:09:45 +0100

    Adds some useful constants

---
+++ src/CONSTANTS.js
@@ -0,0 +1,7 @@
+export const API_KEY = 'qUnEyRXt';
+export const BASE_URL = 'http://www.cineworld.com/api/';
+export const CINEMAS_URL = 'quickbook/cinemas';
+export const DATES_URL = 'quickbook/dates';
+export const FILMS_URL = 'quickbook/films';
+export const PERFORMANCES_URL = 'quickbook/performances';
+export const CINEMA_ID = 61;
